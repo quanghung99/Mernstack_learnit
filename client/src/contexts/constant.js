c3b2aa9b930f06523e719dcdbdb34fd632631c65
quanghung99 <hung99ktb@gmail.com>
2021-10-14 05:01:29 +0700

    deployed

--- client/src/contexts/constant.js
+++ client/src/contexts/constant.js
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.NODE_ENV !== 'production'
-    ? 'http://localhost:5000/api'
+    ? 'https://fast-refuge-62315.herokuapp.com/api'
     : 'somedeployedUrl';
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'project_learnit';
